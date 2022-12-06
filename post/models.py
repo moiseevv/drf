c65e1052a3d1d1e00c1db3e4da307f7d2594fb39
Vladimir Moiseev neo <lutii@ngs.ru>
2022-12-06 15:45:54 +0700

    start

---
+++ post/models.py
@@ -0,0 +1,10 @@
+from django.db import models
+
+class Post(models.Model):
+    author = models.ForeignKey("auth.User", on_delete=models.CASCADE, null = True, black = True)
+    title = models.CharField(max_length=64)
+    text = models.TextField()
+    create_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.title}"
